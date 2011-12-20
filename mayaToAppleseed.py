869b82894d0558cda4df23eae51952c94b692806
Jonathan Topf <jonathan@jonathantopf.com>
2011-12-20 19:38:08 +0000

    added code to launch script from maya

--- mayaToAppleseed.py
+++ mayaToAppleseed.py
@@ -1,16 +1,49 @@
+# mayaToAppleseed.py 
+
+# WORK IN PROGRESS
+
+# uncomment the next few lines replace the path and run them in the maya script editor to launch the script 
+
+#import sys
+#
+#sys.path.append('/projects/mayaToAppleseed')
+#
+#import mayaToAppleseed
+#
+#reload(mayaToAppleseed)
+#
+#mayaToAppleseed.m2s()
+
+
+#
+# laod ui
+#
+
 import maya.cmds as cmds
+import os
 
-matrix = cmds.xform(q=True, ws=True, m=True) #get matrix from selected object
+def m2s():
+    mayaToAppleseedUi = cmds.loadUI(f="{0}/mayaToAppleseed.ui".format(os.path.dirname(__file__)))    
+    cmds.showWindow(mayaToAppleseedUi)
 
-m = [matrix[0],matrix[1],matrix[2],matrix[3]], [matrix[4],matrix[5],matrix[6],matrix[7]], [matrix[8],matrix[9],matrix[10],matrix[11]], [matrix[12],matrix[13],matrix[14],matrix[15]]
+#
+# print matrix
+#
 
-
-print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 0], matrix[ 4], matrix[ 8], matrix[12])
-print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 1], matrix[ 5], matrix[ 9], matrix[13])
-print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 2], matrix[ 6], matrix[10], matrix[14])
-print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 3], matrix[ 7], matrix[11], matrix[15])
-
-#git test again
-         
-         
+def printMatrix():
+	
+	
+	matrix = cmds.xform(q=True, ws=True, m=True) #get matrix from selected object
+	
+	m = [matrix[0],matrix[1],matrix[2],matrix[3]], [matrix[4],matrix[5],matrix[6],matrix[7]], [matrix[8],matrix[9],matrix[10],matrix[11]], [matrix[12],matrix[13],matrix[14],matrix[15]]
+	
+	
+	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 0], matrix[ 4], matrix[ 8], matrix[12])
+	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 1], matrix[ 5], matrix[ 9], matrix[13])
+	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 2], matrix[ 6], matrix[10], matrix[14])
+	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 3], matrix[ 7], matrix[11], matrix[15])
+	
+	#git test again
+			 
+			 
          
