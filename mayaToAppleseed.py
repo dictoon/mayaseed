1be515ae9a0cf9e0ee285896e08d00c63daf3b70
Jonathan Topf <jonathan@jonathantopf.com>
2011-12-21 02:07:30 +0000

    began work on mayaGeometry() object

--- mayaToAppleseed.py
+++ mayaToAppleseed.py
@@ -16,14 +16,8 @@ import maya.cmds as cmds
 import os
 
 #
-# define objects to store data in
+# maya camera object --
 #
-
-# camera
-# assembly
-# obj
-# object
-# material
 
 class mayaCamera():
     name = ""
@@ -41,7 +35,6 @@ class mayaCamera():
         # transpose camera matrix -> XXX0, YYY0, ZZZ0, XYZ1
         m = cmds.getAttr(cam+'.matrix')
         self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
-
     
     def info(self):
         print("name: {0}".format(self.name))
@@ -49,7 +42,42 @@ class mayaCamera():
         print("controller_target: {0}".format(self.controller_target))
         print("film dimensions: {0}".format(self.film_dimensions))
         print("focal length: {0}".format(self.focal_length))
-        print("transform matrix: {0} # XXX0 YYY0 ZZZ0 XYZ1".format(self.transform))
+        print("transform matrix: {0} # XXX0 YYY0 ZZZ0 XYZ1\n".format(self.transform))
+
+#
+# maya geometry object --
+#
+
+class mayaGeometry():
+    name = ""
+    transform = [1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1] # XXX0, YYY0, ZZZ0, XYZ1
+    outPutFile = ""
+    assembly = "assembly"
+    
+    def __init__(self,obj):
+        self.name = obj    
+	
+	
+
+
+#
+# read maya scene and generate objects --
+#
+
+# create and populate a list of mayaCamera objects
+cam_list = []
+for c in cmds.ls(ca=True, v=True):
+    cam_list.append(mayaCamera(c))
+
+# create and polulate a list of mayaGeometry objects
+shape_list = cmds.ls(g=True, v=True) # get maya geometry
+geo_transform_list = []
+for g in shape_list:
+    geo_transform_list.append(cmds.listRelatives(g, ad=True, ap=True)[0]) # add first connected transform to the list
+
+    
+
+
 
 
 
@@ -62,10 +90,6 @@ def m2s():
     mayaToAppleseedUi = cmds.loadUI(f="{0}/mayaToAppleseed.ui".format(os.path.dirname(__file__)))    
     cmds.showWindow(mayaToAppleseedUi)
 
-
-#
-# return list of cameras
-#
 
 #
 # return list of assemblies
