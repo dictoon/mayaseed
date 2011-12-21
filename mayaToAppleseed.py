43964670917e2900163222973e82cf952893d0ef
Jonathan Topf <jonathan@jonathantopf.com>
2011-12-21 00:51:27 +0000

    added camera object to mayaTo Appleseed and updated ui

--- mayaToAppleseed.py
+++ mayaToAppleseed.py
@@ -1,49 +1,75 @@
 # mayaToAppleseed.py 
 
-# WORK IN PROGRESS
+# WORK IN PROGRESS!!
 
 # uncomment the next few lines replace the path and run them in the maya script editor to launch the script 
-
+#--------------------maya command (python)--------------------
 #import sys
-#
 #sys.path.append('/projects/mayaToAppleseed')
-#
 #import mayaToAppleseed
-#
 #reload(mayaToAppleseed)
-#
 #mayaToAppleseed.m2s()
+#-------------------------------------------------------------
 
-
-#
-# laod ui
-#
 
 import maya.cmds as cmds
 import os
+
+#
+# define objects to store data in
+#
+
+# camera
+# assembly
+# obj
+# object
+# material
+
+class mayaCamera():
+    name = ""
+    model = "pinhole_camera"
+    controller_target = [0, 0, 0]
+    film_dimensions = [1.417 , 0.945]
+    focal_length = 35.000
+    transform = [1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1] # XXX0, YYY0, ZZZ0, XYZ1
+   
+    def __init__(self, cam):
+        self.name = cam
+        self.film_dimensions[0] = cmds.getAttr(cam+'.horizontalFilmAperture')
+        self.film_dimensions[1] = cmds.getAttr(cam+'.verticalFilmAperture')
+        self.focal_length = cmds.getAttr(cam+'.focalLength')
+        # transpose camera matrix -> XXX0, YYY0, ZZZ0, XYZ1
+        m = cmds.getAttr(cam+'.matrix')
+        self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
+
+    
+    def info(self):
+        print("name: {0}".format(self.name))
+        print("camera model: {0}".format(self.model))
+        print("controller_target: {0}".format(self.controller_target))
+        print("film dimensions: {0}".format(self.film_dimensions))
+        print("focal length: {0}".format(self.focal_length))
+        print("transform matrix: {0} # XXX0 YYY0 ZZZ0 XYZ1".format(self.transform))
+
+
+
+
+#
+# laod ui --
+#
 
 def m2s():
     mayaToAppleseedUi = cmds.loadUI(f="{0}/mayaToAppleseed.ui".format(os.path.dirname(__file__)))    
     cmds.showWindow(mayaToAppleseedUi)
 
+
 #
-# print matrix
+# return list of cameras
 #
 
-def printMatrix():
-	
-	
-	matrix = cmds.xform(q=True, ws=True, m=True) #get matrix from selected object
-	
-	m = [matrix[0],matrix[1],matrix[2],matrix[3]], [matrix[4],matrix[5],matrix[6],matrix[7]], [matrix[8],matrix[9],matrix[10],matrix[11]], [matrix[12],matrix[13],matrix[14],matrix[15]]
-	
-	
-	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 0], matrix[ 4], matrix[ 8], matrix[12])
-	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 1], matrix[ 5], matrix[ 9], matrix[13])
-	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 2], matrix[ 6], matrix[10], matrix[14])
-	print "{0:.16f} {1:.16f} {2:.16f} {3:.16f}".format(matrix[ 3], matrix[ 7], matrix[11], matrix[15])
-	
-	#git test again
-			 
-			 
-         
+#
+# return list of assemblies
+#
+
+
+
