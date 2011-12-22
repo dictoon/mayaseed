2583369fa37e671694a5b7f14a621826d52345a1
Jonathan Topf <jonathan@jonathantopf.com>
2011-12-22 00:16:30 +0000

    added object class and began material class

--- mayaToAppleseed.py
+++ mayaToAppleseed.py
@@ -16,10 +16,32 @@ import maya.cmds as cmds
 import os
 
 #
+# maya shader object --
+#
+
+class mayaShader(): #object transform name
+    name = ""
+    color = [0.5,0.5,0.5]
+    color_texture = ""
+    specular_color = [0.5,0.5,0.5]
+    specular_color_texture = ""
+    
+    def __init__(self, obj): 
+        #get shader name from transform name
+        shape = cmds.listRelatives(obj, s=True)[0]
+        shadingEngine = cmds.listConnections(shape, t='shadingEngine')[0]
+        shader = cmds.connectionInfo((shadingEngine + ".surfaceShader"),sourceFromDestination=True).split('.')[0] #find the attribute the surface shader is plugged into athen split off the attribute name to leave the shader name
+        self.name = shader
+        
+        
+
+
+
+#
 # maya camera object --
 #
 
-class mayaCamera():
+class mayaCamera(): #(camera_name)
     name = ""
     model = "pinhole_camera"
     controller_target = [0, 0, 0]
@@ -48,16 +70,40 @@ class mayaCamera():
 # maya geometry object --
 #
 
-class mayaGeometry():
+class mayaGeometry(): # (object_transfrm_name, obj_file)
     name = ""
-    transform = [1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1] # XXX0, YYY0, ZZZ0, XYZ1
     outPutFile = ""
     assembly = "assembly"
+    shader = ""
+    transform = [1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1] # XXX0, YYY0, ZZZ0, XYZ1
     
-    def __init__(self,obj):
-        self.name = obj    
+    def __init__(self,obj, obj_file):
+        self.name = obj
+        self.outPutFile = obj_file
+        self.assembly = cmds.listSets(object=obj)[0]
+        # get shader name
+        shape = cmds.listRelatives(obj, s=True)[0]
+        shadingEngine = cmds.listConnections(shape, t='shadingEngine')[0]
+        shader_name = cmds.connectionInfo((shadingEngine + ".surfaceShader"),sourceFromDestination=True).split('.')[0] #find the attribute the surface shader is plugged into athen split off the attribute name to leave the shader name
+        self.shader = shader_name
+        # transpose camera matrix -> XXX0, YYY0, ZZZ0, XYZ1
+        m = cmds.getAttr(obj+'.matrix')
+        self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
+        
+    def info(self):
+        print("name: {0}".format(self.name))
+        print("output file: {0}".format(self.outPutFile))
+        print("assembly name: {0}".format(self.assembly))
+        print("shader name: {0}".format(self.shader))
+        print("transform matrix: {0} # XXX0 YYY0 ZZZ0 XYZ1\n".format(self.transform))
 	
-	
+
+
+
+
+
+
+
 
 
 #
@@ -74,14 +120,6 @@ shape_list = cmds.ls(g=True, v=True) # get maya geometry
 geo_transform_list = []
 for g in shape_list:
     geo_transform_list.append(cmds.listRelatives(g, ad=True, ap=True)[0]) # add first connected transform to the list
-
-    
-
-
-
-
-
-
 #
 # laod ui --
 #
