a0a51a315d9b5ae332ad9ff0cc43c2decb1c365d
Jonathan Topf <jonathan@jonathantopf.com>
2012-01-06 20:46:41 +0000

    light colour export is now working, transforms now export scale value, colour class added, added modified writeXML to automatically ass </>  xml tags and added writeXML.writeLine for lines without tags

--- mayaToAppleseed.py
+++ mayaToAppleseed.py
@@ -35,6 +35,7 @@ def getMayaParams():
     params['outputDir'] = cmds.textField('m2s_outputDir', query=True, text=True)
     params['fileName'] = cmds.textField('m2s_fileName', query=True, text=True)
     params['verbose'] = cmds.checkBox('m2s_verbose', query=True, value=True)
+    params['scene_scale'] = 1
     #scene
     #cameras
     params['sceneCameraExportAllCameras'] = cmds.checkBox('m2s_sceneCameraExportAllCameras', query=True, value=True)
@@ -83,7 +84,53 @@ def getDir(field_name):
         cmds.textField(field_name, edit=True, text=new_state[0])
 
 #
-# maya shader object --
+# color object ---
+#
+
+class color():
+    def __init__(self, name, color, multiplier):
+        self.name = name
+        self.color = [color[0][0],color[0][1],color[0][2]]
+        self.multiplier = multiplier
+        self.color_space = 'srgb'
+        self.wavelength_range = '400.0 700.0'
+        self.alpha = 1.0
+    def writeXML(self, doc):
+        doc.startElement('color name="{0}"'.format(self.name))
+        doc.appendElement('parameter name="color" value="{0:.6f} {1:.6f} {2:.6f}"'.format(self.color[0], self.color[1], self.color[2]))
+        doc.appendElement('parameter name="color_space" value="{0}"'.format(self.color_space))
+        doc.appendElement('parameter name="multiplier" value="{0}"'.format(self.multiplier))
+        doc.appendElement('parameter name="alpha" value="{0}"'.format(self.alpha))
+        doc.startElement('values')
+        doc.appendLine('{0:.6f} {1:.6f} {2:.6f}'.format(self.color[0], self.color[1], self.color[2]))
+        doc.endElement('values')
+        doc.startElement('alpha')
+        doc.appendLine('{0:.6f}'.format(self.alpha))
+        doc.endElement('alpha')
+        doc.endElement('color')
+
+#
+# light object --
+#
+
+class light():
+    def __init__(self, params, name):
+        self.params = params
+        self.name = name
+        self.color_name = self.name + '_exitance'
+        self.color = cmds.getAttr(self.name+'.color')
+        self.multiplier = cmds.getAttr(self.name+'.intensity')
+        self.decay = cmds.getAttr(self.name+'.decayRate')
+        m = cmds.getAttr(self.name+'.matrix')
+        self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
+    def writeXML(self, doc):
+        doc.startElement('light name="{0}" model="point_light"'.format(self.name))
+        doc.appendElement('parameter name="exitance" value="{0}"'.format(self.color_name))
+        writeTransform(doc, 1, self.transform)
+        doc.endElement('light')
+
+#
+# shader object --
 #
 
 class material(): #object transform name
@@ -128,7 +175,7 @@ class material(): #object transform name
             self.edf_texture = None
             
 #
-# maya camera object --
+# camera object --
 #
 
 class camera(): #(camera_name)
@@ -137,7 +184,7 @@ class camera(): #(camera_name)
         if self.params['sceneCameraDefaultThinLens'] or cmds.getAttr(cam+'.depthOfField'):
             self.model = 'thinlens_camera'
             self.f_stop = cmds.getAttr(cam+'.fStop')
-            self.focal_distance = cmds.getAttr(cam+'.focusDistance') * inch_to_meter
+            self.focal_distance = cmds.getAttr(cam+'.focusDistance') /10
             self.diaphram_blades = 0
             self.diaphram_tilt_angle = 0.0
         else:
@@ -152,21 +199,21 @@ class camera(): #(camera_name)
         self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
    
     def writeXML(self, doc):
-        doc.startElement('<camera name="{0}" model="{1}">'.format(self.name, self.model))
-        doc.appendElement('<parameter name="film_dimensions" value="{0} {1}"/>'.format(self.horizontal_fov, self.vertical_fov))
-        doc.appendElement('<parameter name="focal_length" value="{0}" />'.format(self.focal_length))
+        doc.startElement('camera name="{0}" model="{1}"'.format(self.name, self.model))
+        doc.appendElement('parameter name="film_dimensions" value="{0} {1}"'.format(self.horizontal_fov, self.vertical_fov))
+        doc.appendElement('parameter name="focal_length" value="{0}"'.format(self.focal_length))
         if self.model == 'thinlens_camera':
             print('exporting ' + self.name + ' as thinlens camera attribs')
-            doc.appendElement('<parameter name="focal_distance" value="{0}" />'.format(self.focal_distance))
-            doc.appendElement('<parameter name="f_stop" value="{0}" />'.format(self.f_stop))
-            doc.appendElement('<parameter name="diaphragm_blades" value="{0}" />'.format(self.diaphram_blades))
-            doc.appendElement('<parameter name="diaphragm_tilt_angle" value="{0}"/>'.format(self.diaphram_tilt_angle))
+            doc.appendElement('parameter name="focal_distance" value="{0}"'.format(self.focal_distance))
+            doc.appendElement('parameter name="f_stop" value="{0}"'.format(self.f_stop))
+            doc.appendElement('parameter name="diaphragm_blades" value="{0}"'.format(self.diaphram_blades))
+            doc.appendElement('parameter name="diaphragm_tilt_angle" value="{0}"'.format(self.diaphram_tilt_angle))
         #output transform matrix
-        writeTransform(doc, self.transform)
-        doc.endElement('</camera>')
+        writeTransform(doc, self.params['scene_scale'], self.transform)
+        doc.endElement('camera')
 
 #
-# maya geometry object --
+# geometry object --
 #
 
 class geometry(): # (object_transfrm_name, obj_file)
@@ -184,14 +231,14 @@ class geometry(): # (object_transfrm_name, obj_file)
         self.transform = [m[0],m[1],m[2],m[3]], [m[4],m[5],m[6],m[7]], [m[8],m[9],m[10],m[11]], [m[12],m[13],m[14],m[15]]
         
     def writeXMLObject(self, doc):
-        doc.startElement('<object name="{0}" model="mesh_object">'.format(self.assembly))
-        doc.appendElement('<parameter name="filename" value="{0}"/>'.format(self.output_file))
-        doc.endElement('</object>')
+        doc.startElement('object name="{0}" model="mesh_object"'.format(self.assembly))
+        doc.appendElement('parameter name="filename" value="{0}"'.format(self.output_file))
+        doc.endElement('object>')
     def writeXMLInstance(self, doc):
-        doc.startElement('<object_instance name="{0}_inst" object="{1}.{2}">'.format(self.name, self.assembly, self.name,))
+        doc.startElement('object_instance name="{0}_inst" object="{1}.{2}"'.format(self.name, self.assembly, self.name,))
         writeTransform(doc)
         #doc.appendElement('<assign_material slot="0" material="{0}"/>'.format(self.material))
-        doc.endElement('</object_instance>')
+        doc.endElement('object_instance')
 #
 # assembly object --
 #
@@ -200,9 +247,27 @@ class assembly():
     def __init__(self, params, name='main_assembly'):
         self.params = params
         self.name = name
+        self.light_objects = []
         self.geo_objects = []
         self.mat_list = []
         self.mat_objects = []
+        self.color_objects = []
+        self.shader_objects = []
+        self.bsdf_objects = []
+        self.edf_objects = []
+
+        #if name is default populate list with all lights otherwise just lights from set with the same name as the object
+        if (self.name == 'main_assembly'):
+            for light_shape in cmds.ls(lights=True):
+                self.light_objects.append(light(self.params, cmds.listRelatives(light_shape, ad=True, ap=True)[0]))
+        else:
+            for light_shape in cmds.listRelatives('set1', typ='light'):
+                self.light_objects.append(light(self.params, cmds.listRelatives(light_shape, ad=True, ap=True)[0]))
+        #add light colors to list
+        for light_object in self.light_objects:
+            self.color_objects.append(color((light_object.color_name), light_object.color, light_object.multiplier))
+        
+        
         #if name is default populate list with all geometry otherwise just geometry from set with the same name as the object
         if (self.name == 'main_assembly'):
             #create a list of all geometry objects and itterate over them
@@ -212,31 +277,41 @@ class assembly():
         else:
             for obj in cmds.listConnections(self.name, sh=True):
                 self.geo_objects.append(geometry(obj, ('geo/'+self.name+'.obj'), self.name	))
+                
         #populate list with individual materials
         for geo in self.geo_objects:
             if not geo.material in self.mat_list:
                 print ('adding ' + geo.material + ' to mat_list')
                 self.mat_list.append(geo.material)
+                
         #populate list with material objects        
         for mat in self.mat_list:
             self.mat_objects.append(material(mat))
         
     def writeXML(self, doc):
-        doc.startElement('<assembly name="{0}">'.format(self.name))
+        doc.startElement('assembly name="{0}"'.format(self.name))
+
+        #write colors
+        for col in self.color_objects:
+             col.writeXML(doc)
         
         #write .obj object
-        doc.startElement('<object name="{0}" model="mesh_object">'.format(self.name))
-        doc.appendElement('<parameter name="filename" value="geo/{0}.obj"/>'.format(self.name))
-        doc.endElement('</object>')
+        doc.startElement('object name="{0}" model="mesh_object"'.format(self.name))
+        doc.appendElement('parameter name="filename" value="geo/{0}.obj"'.format(self.name))
+        doc.endElement('object')
+        
+        #write lights
+        for light_name in self.light_objects:
+            light_name.writeXML(doc)
         
         #write geo object instances
         for geo in self.geo_objects:
             geo.writeXMLInstance(doc)
         
-        doc.endElement('</assembly>')
-        doc.startElement('<assembly_instance name="{0}_inst" assembly="{1}">'.format(self.name, self.name))
-        writeTransform(doc)
-        doc.endElement('</assembly_instance>')
+        doc.endElement('assembly')
+        doc.startElement('assembly_instance name="{0}_inst" assembly="{1}"'.format(self.name, self.name))
+        writeTransform(doc, self.params['scene_scale'])
+        doc.endElement('assembly_instance')
         
         #select and export objects
         cmds.select(cl=True)
@@ -257,7 +332,7 @@ class scene():
         self.params = params
         self.assembly_list = []
     def writeXML(self, doc):
-        doc.startElement('<scene>')
+        doc.startElement('scene')
         #write cameras
         if self.params['sceneCameraExportAllCameras']:
             #export all cameras
@@ -294,7 +369,7 @@ class scene():
         else:
             new_assembly = assembly(self.params, 'main_assembly')
             new_assembly.writeXML(doc)
-        doc.endElement('</scene>')
+        doc.endElement('scene')
 
 #
 # output object --
@@ -304,13 +379,13 @@ class output():
     def __init__(self, params):
         self.params = params
     def writeXML(self, doc):
-        doc.startElement('<output>')
-        doc.startElement('<frame name="beauty">')
-        doc.appendElement('<parameter name="camera" value="{0}" />'.format(self.params['outputCamera']))
-        doc.appendElement('<parameter name="color_space" value="{0}" />'.format(self.params['outputColorSpace']))
-        doc.appendElement('<parameter name="resolution" value="{0} {1}" />'.format(self.params['outputResWidth'], self.params['outputResHeight']))
-        doc.endElement('</frame>')
-        doc.endElement('</output>')
+        doc.startElement('output')
+        doc.startElement('frame name="beauty"')
+        doc.appendElement('parameter name="camera" value="{0}"'.format(self.params['outputCamera']))
+        doc.appendElement('parameter name="color_space" value="{0}"'.format(self.params['outputColorSpace']))
+        doc.appendElement('parameter name="resolution" value="{0} {1}"'.format(self.params['outputResWidth'], self.params['outputResHeight']))
+        doc.endElement('frame')
+        doc.endElement('output')
 
 #
 # configurations object --
@@ -320,38 +395,38 @@ class configurations():
     def __init__(self, params):
         self.params = params
     def writeXML(self,doc):
-        doc.startElement("<configurations>")
+        doc.startElement("configurations")
         #if 'customise interactive configuration' is set read customised values
         if self.params['customInteractiveConfigCheck']:
-            doc.startElement('<configuration name="interactive" base="base_interactive">')
+            doc.startElement('configuration name="interactive" base="base_interactive"')
             engine = ''
             if self.params['customInteractiveConfigEngine'] == 'Path Tracing':
                 engine = "pt"
             else:
                 engine = "drt"
-            doc.appendElement('<parameter name="lighting_engine" value="{0}"/>'.format(engine))
-            doc.appendElement('<parameter name="min_samples" value="{0}" />'.format(self.params['customInteractiveConfigMinSamples']))
-            doc.appendElement('<parameter name="max_samples" value="{0}" />'.format(self.params['customInteractiveConfigMaxSamples']))
-            doc.endElement('</configuration>')
+            doc.appendElement('parameter name="lighting_engine" value="{0}"'.format(engine))
+            doc.appendElement('parameter name="min_samples" value="{0}"'.format(self.params['customInteractiveConfigMinSamples']))
+            doc.appendElement('parameter name="max_samples" value="{0}"'.format(self.params['customInteractiveConfigMaxSamples']))
+            doc.endElement('configuration')
         else:# otherwise add default configurations
-            doc.appendElement('<configuration name="interactive" base="base_interactive" />')
+            doc.appendElement('configuration name="interactive" base="base_interactive"')
   
         #if 'customise final configuration' is set read customised values
         if cmds.checkBox('m2s_customFinalConfigCheck', query=True, value=True) == True:
-            doc.startElement('<configuration name="final" base="base_final">')
+            doc.startElement('configuration name="final" base="base_final"')
             engine = ''
             if cmds.optionMenu('m2s_customFinalConfigEngine', query=True, value=True) == "Path Tracing":
                 engine = 'pt'
             else:
                 engine = 'drt'
-            doc.appendElement('<parameter name="lighting_engine" value="{0}"/>'.format(engine))
-            doc.appendElement('<parameter name="min_samples" value="{0}" />'.format(self.params['customFinalConfigMinSamples']))
-            doc.appendElement('<parameter name="max_samples" value="{0}" />'.format(self.params['customFinalConfigMaxSamples']))
-            doc.endElement("</configuration>")
+            doc.appendElement('parameter name="lighting_engine" value="{0}"'.format(engine))
+            doc.appendElement('parameter name="min_samples" value="{0}"'.format(self.params['customFinalConfigMinSamples']))
+            doc.appendElement('parameter name="max_samples" value="{0}"'.format(self.params['customFinalConfigMaxSamples']))
+            doc.endElement("configuration")
         else:# otherwise add default configurations
-            doc.appendElement('<configuration name="final" base="base_final" />')
+            doc.appendElement('configuration name="final" base="base_final"')
         # begin adding custom configurations
-        doc.endElement('</configurations>')
+        doc.endElement('configurations')
 	
 #
 # writeXml Object --
@@ -371,15 +446,18 @@ class writeXml(): #(file_path)
             return
         
     def startElement(self,str):
-        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * " ") + str + "\n")
+        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * ' ') + "<" + str + '>\n')
         self.indentation_level += 1
         
     def endElement(self, str):
         self.indentation_level -= 1
-        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * " ") + str + "\n")
+        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * ' ') + '</' + str + '>\n')
         
     def appendElement(self, str):
-        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * " ") + str + "\n")
+        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * ' ') + '<' + str + '/>\n')
+        
+    def appendLine(self, str):
+        self.file_object.write(((self.indentation_level * self.spaces_per_indentation_level) * ' ') + str + '\n')
         
     def close(self):
         self.file_object.close() #close file
@@ -388,17 +466,18 @@ class writeXml(): #(file_path)
 # writeTransform function --
 #
 
-def writeTransform(doc, transform = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]):
-    doc.startElement('<transform>')
-    doc.startElement('<matrix>')
+def writeTransform(doc, scale = 1, transform = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]):
+    doc.startElement('transform')
+    doc.appendElement('scaling value="{0}"'.format(scale))
+    doc.startElement('matrix')
 
-    doc.appendElement('{0:.16f} {1:.16f} {2:.16f} {3:.16f}'.format(transform[0][0], transform[1][0], transform[2][0], transform[3][0]))
-    doc.appendElement('{0:.16f} {1:.16f} {2:.16f} {3:.16f}'.format(transform[0][1], transform[1][1], transform[2][1], transform[3][1]))
-    doc.appendElement('{0:.16f} {1:.16f} {2:.16f} {3:.16f}'.format(transform[0][2], transform[1][2], transform[2][2], transform[3][2]))
-    doc.appendElement('{0:.16f} {1:.16f} {2:.16f} {3:.16f}'.format(transform[0][3], transform[1][3], transform[2][3], transform[3][3]))	
+    doc.appendLine('{0:.15f} {1:.15f} {2:.15f} {3:.15f}'.format(transform[0][0], transform[1][0], transform[2][0], transform[3][0]))
+    doc.appendLine('{0:.15f} {1:.15f} {2:.15f} {3:.15f}'.format(transform[0][1], transform[1][1], transform[2][1], transform[3][1]))
+    doc.appendLine('{0:.15f} {1:.15f} {2:.15f} {3:.15f}'.format(transform[0][2], transform[1][2], transform[2][2], transform[3][2]))
+    doc.appendLine('{0:.15f} {1:.15f} {2:.15f} {3:.15f}'.format(transform[0][3], transform[1][3], transform[2][3], transform[3][3]))	
 
-    doc.endElement('</matrix>')
-    doc.endElement('</transform>')
+    doc.endElement('matrix')
+    doc.endElement('transform')
 
 #
 # build and export
@@ -409,9 +488,9 @@ def export():
     if not params['error']:
         print('--exporting to appleseed--')
         doc = writeXml('{0}/{1}'.format(params['outputDir'], params['fileName']))
-        doc.appendElement('<?xml version="1.0" encoding="UTF-8"?>') # XML format string
-        doc.appendElement('<!-- File generated by {0} version {1} visit {2} for more info -->'.format(script_name, version, more_info_url))
-        doc.startElement('<project>')
+        doc.appendLine('<?xml version="1.0" encoding="UTF-8"?>') # XML format string
+        doc.appendLine('<!-- File generated by {0} version {1} visit {2} for more info and the latest super exciting release!-->'.format(script_name, version, more_info_url))
+        doc.startElement('project')
         scene_element = scene(params)
         scene_element.writeXML(doc)
         output_element = output(params)
@@ -419,7 +498,7 @@ def export():
         config_element = configurations(params)
         config_element.writeXML(doc)
     
-        doc.endElement('</project>')
+        doc.endElement('project')
         doc.close()
         print ('--export finished--')
     else:
