eb31f03590d71892d4b7d0e246a9eb2a7a317eea
Francois Beaune <beaune@aist.enst.fr>
2013-01-30 15:48:49 +0100

    fixed regression: the installation script wouldn't properly set the environment variables on Windows.

--- install_helper.py
+++ install_helper.py
@@ -2,6 +2,10 @@ import sys
 import os
 import maya.cmds as cmds
 
+def append_env(variable, path):
+    separator = ';' if sys.platform == 'win32' or sys.platform == 'win64' else ':'
+    return 'putenv "{0}" (`getenv "{0}"` + \"{1}{2}\");'.format(variable, separator, path.replace('\\', '/'))
+
 def install(userSetup_file, install_dir):
     installation_name = 'mayaseed'
 
@@ -21,16 +25,14 @@ def install(userSetup_file, install_dir):
       if not inside_block and not is_block_delimiter:
         file.write(line + '\n')
 
-    separator = ';' if sys.platform == 'win32' or sys.platform == 'win64' else ':'
-
     file.write('// ' + installation_name + '  -------------------------------------------------------------------------------\n')
     file.write('\n')
-    file.write('putenv "MAYA_SCRIPT_PATH" (`getenv "MAYA_SCRIPT_PATH"` + \"{0}{1}\");\n'.format(separator, os.path.join(install_dir, 'scripts')))
-    file.write('putenv "MAYA_SCRIPT_PATH" (`getenv "MAYA_SCRIPT_PATH"` + \"{0}{1}\");\n'.format(separator, os.path.join(install_dir, 'graphics')))
-    file.write('putenv "MAYA_PLUG_IN_PATH" (`getenv "MAYA_PLUG_IN_PATH"` + \"{0}{1}\");\n'.format(separator, os.path.join(install_dir, 'plugins')))
+    file.write(append_env("MAYA_SCRIPT_PATH", os.path.join(install_dir, 'scripts')) + '\n')
+    file.write(append_env("MAYA_SCRIPT_PATH", os.path.join(install_dir, 'graphics')) + '\n')
+    file.write(append_env("MAYA_PLUG_IN_PATH", os.path.join(install_dir, 'plugins')) + '\n')
     file.write('\n')
     file.write('// ' + installation_name + '  -------------------------------------------------------------------------------\n')
 
     file.close()
 
-    cmds.confirmDialog(title=installation_name + ' install', message='All done! Just restart Maya and enable any plugins not already enabled in the plugin manager.', button='OK')
+    cmds.confirmDialog(title=installation_name + ' installation', message='All done! Just restart Maya and enable any plugins not already enabled in the plugin manager.', button='OK')
